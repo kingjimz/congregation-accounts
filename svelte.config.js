aebc65b72c49e8a8d77d5ae38c9e6672df154479
kingjimz <kingjimz>
2025-07-23 21:59:07 +0800

    setup continuous deployment config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +8,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// Using Netlify adapter for continuous deployment
+		adapter: adapter({
+			// Optional: configure Netlify-specific options
+			edge: false, // Set to true if you want to use Netlify Edge Functions
+			split: false // Set to true to split your app into multiple functions
+		})
 	}
 };
 
